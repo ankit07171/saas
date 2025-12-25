6a65135b2bf1ba498056e9844adb5ad0b6b14270
akt.07171@gmail.com <ankit11042005@gmail.com>
2025-12-25 15:42:46 +0530

    home pg

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,32 +1,37 @@
-import React from 'react'
-import { assets } from '../assets/assets'
-import { useNavigate } from 'react-router-dom'
-import { ArrowRight } from 'lucide-react'
-
+import React from "react";
+import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
+import { ArrowRight } from "lucide-react";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 const Navbar = () => {
-  const navigate = useNavigate()
-
+  const navigate = useNavigate();
+  const { user } = useUser();
+  const { openSignIn } = useClerk();
   return (
-    <div className='fixed z-50 w-full backdrop-blur-xl flex justify-between
-      items-center py-3 px-4 sm:px-20 xl:px-32'>
-
+    <div
+      className="fixed z-50 w-full backdrop-blur-xl flex justify-between
+      items-center py-3 px-4 sm:px-20 xl:px-32"
+    >
       <img
         src={assets.logo}
         alt="logo"
-        className='w-32 sm:w-44 cursor-pointer'
-        onClick={() => navigate('/')}
+        className="w-32 sm:w-44 cursor-pointer"
+        onClick={() => navigate("/")}
       />
 
-      <button
-        onClick={() => navigate('/ai')}
-        className='flex items-center gap-2 rounded-full text-sm
-        cursor-pointer bg-blue-300 text-black px-10 py-2.5'
-      >
-        Get started <ArrowRight className='w-4 h-4' />
-      </button>
-
+      {user ? (
+        <UserButton />
+      ) : (
+        <button
+          onClick={openSignIn}
+          className="flex items-center gap-2 rounded-full text-sm
+        cursor-pointer bg-primary text-white px-10 py-2.5"
+        >
+          Get started <ArrowRight className="w-4 h-4" />
+        </button>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import AiTools from '../components/AiTools'
 
 export default function Home() {
   return (
     <>
     <Navbar/>
- 
+ <Hero/>
+ <AiTools/>
     </>
   )
 }
