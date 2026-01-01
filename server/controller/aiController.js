78e56db98fd25d726eaf4b1fd498b184ad062952
akt.07171@gmail.com <ankit11042005@gmail.com>
2026-01-02 01:44:28 +0530

    free plans backend

--- server/controller/aiController.js
+++ server/controller/aiController.js
@@ -1,67 +1,17 @@
-// import OpenAI from "openai";
-// import sql from "../config/db.js";
-// import { clerkClient } from "@clerk/express";
-
-// const AI = new OpenAI({
-//     apiKey: "process.env.GEMINI_API_KEY",
-//     baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/"
-// });
-
-// export const generateArticles = async(req,res)=>{
-// try {
-//     const {userId} = req.auth()
-//     const {length ,prompt} = req.body
-//     const plan =req.plan;
-//     const free_usage  = req.free_usage;
-    
-//     if(plan !== 'premium' && free_usage>=10){
-//         return res.json({success:false , message:'Limit  reached. Upgrade to continue.'})
-//     }
-
-//     const content = response.choices[0].message.content
-
-//     await sql `IMPORT INTO Creations (userId,prompt,content,type)
-//     VALUES(${userId},${prompt},${content},'article')`
-
-//  if(plan !== 'premium'){
-//          await clerkClient.users.updateUserMetadata(userId,{
-//             privateMetadata:{free_usage: free_usage+1}
-//          }
-//          )
-//     }
-//     res.json({success:true,content})
-
-// } catch (error) {
-//     res.json({success:false,error:error.message})
-// }
-// }
-
-
-// const response = await AI.chat.completions.create({
-//     model: "gemini-2.5-flash",
-//     messages: [
-//         {
-//             role: "user",
-//             content: prompt,
-//         },
-//     ],
-//     temperature: 0.7,
-//     max_tokens: length,
-// });
-
-// console.log(response.choices[0].message);
-import OpenAI from "openai";
+import { GoogleGenAI } from "@google/genai";
 import sql from "../config/db.js";
 import { clerkClient } from "@clerk/express";
 
-const AI = new OpenAI({
-  apiKey: process.env.GEMINI_API_KEY, 
-  baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/",
+// Gemini client
+const ai = new GoogleGenAI({
+  apiKey: process.env.GEMINI_API_KEY,
 });
 
 export const generateArticles = async (req, res) => {
   try {
-    const { userId } = req.auth();
+    const authData = await req.auth();
+    const userId = authData.userId;
+
     const { length, prompt } = req.body;
     const plan = req.plan;
     const free_usage = req.free_usage;
@@ -77,24 +27,15 @@ export const generateArticles = async (req, res) => {
       });
     }
 
-    
-    const response = await AI.chat.completions.create({
+    const response = await ai.models.generateContent({
       model: "gemini-2.5-flash",
-      messages: [
-        {
-          role: "user",
-          content: prompt,
-        },
-      ],
-      temperature: 0.7,
-      max_tokens: length,
+      contents: prompt,
     });
 
-    const content = response.choices[0].message.content;
+    const content = response.text;
 
-    
     await sql`
-      INSERT INTO creations (userid, prompt, content, type)
+      INSERT INTO creations (user_id, prompt, content, type)
       VALUES (${userId}, ${prompt}, ${content}, 'article')
     `;
 
@@ -107,8 +48,106 @@ export const generateArticles = async (req, res) => {
     }
 
     res.json({ success: true, content });
+
   } catch (error) {
-    console.error(error);
-    res.json({ success: false, error: error.message });
+    console.error("Gemini Error:", error);
+    res.status(500).json({ success: false, error: error.message });
   }
 };
+
+export const generateBlogTitles = async (req, res) => {
+  try {
+    const authData = await req.auth();
+    const userId = authData.userId;
+
+    const { prompt } = req.body;
+    const plan = req.plan;
+    const free_usage = req.free_usage;
+
+    if (!prompt) {
+      return res.json({ success: false, message: "Prompt and length required" });
+    }
+
+    if (plan !== "premium" && free_usage >= 10) {
+      return res.json({
+        success: false,
+        message: "Limit reached. Upgrade to continue.",
+      });
+    }
+
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: prompt,
+    });
+
+    const content = response.text;
+
+    await sql`
+      INSERT INTO creations (user_id, prompt, content, type)
+      VALUES (${userId}, ${prompt}, ${content}, 'blog-title')
+    `;
+
+    if (plan !== "premium") {
+      await clerkClient.users.updateUserMetadata(userId, {
+        privateMetadata: {
+          free_usage: free_usage + 1,
+        },
+      });
+    }
+
+    res.json({ success: true, content });
+
+  } catch (error) {
+    console.error("Gemini Error:", error);
+    res.status(500).json({ success: false, error: error.message });
+  }
+};
+
+export const generateImage = async (req, res) => {
+  try {
+    const authData = await req.auth();
+    const userId = authData.userId;
+
+    const { prompt, publish } = req.body;
+    const plan = req.plan;
+    // const free_usage = req.free_usage;
+
+    if (!prompt) {
+      return res.json({ success: false, message: "Prompt and length required" });
+    }
+
+    if (plan !== "premium" ) {
+      return res.json({
+        success: false,
+        message: "This plan is available only for premium subsciptions.",
+      });
+    }
+
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: prompt,
+    });
+
+    const content = response.text;
+
+    await sql`
+      INSERT INTO creations (user_id, prompt, content, type)
+      VALUES (${userId}, ${prompt}, ${content}, 'blog-title')
+    `;
+
+    if (plan !== "premium") {
+      await clerkClient.users.updateUserMetadata(userId, {
+        privateMetadata: {
+          free_usage: free_usage + 1,
+        },
+      });
+    }
+
+    res.json({ success: true, content });
+
+  } catch (error) {
+    console.error("Gemini Error:", error);
+    res.status(500).json({ success: false, error: error.message });
+  }
+};
+
