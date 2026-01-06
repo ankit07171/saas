2fe9ff58a2350bf373e782334c81235f132ffb6f
akt.07171@gmail.com <ankit11042005@gmail.com>
2026-01-06 20:35:15 +0530

    multer upd

--- server/config/multer.js
+++ server/config/multer.js
@@ -1,3 +1,7 @@
 import multer from "multer";
 
-export const upload = multer({ dest: "uploads/" });
+const storage = multer.memoryStorage();
+
+const upload = multer({ storage });
+
+export default upload;
